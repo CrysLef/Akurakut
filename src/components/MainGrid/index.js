d1b86eebe17328f733194546b8fe2d866a7aec71
Crystian Lefundes <crystian.lf@gmail.com>
2021-07-19 18:26:28 -0300

    Add styles to MainGrid

--- src/components/MainGrid/index.js
+++ src/components/MainGrid/index.js
@@ -2,13 +2,25 @@ import styled from 'styled-components'
 
 
 const MainGrid = styled.main `
-  display: grid;
+  width: 100%;
   grid-gap: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 500px;
   padding: 16px;
-  margin: 0 auto;
 
+  .profileArea {
+    display: none;
+    @media(min-width: 860px) {
+      display: block;
+    }
+
+  }
   @media(min-width: 860px) {
-    grid-template-areas: "profileArea welcomeArea profileRelationArea";
+    max-width: 1110px;
+    display: grid;
+    grid-template-areas: 
+      "profileArea welcomeArea profileRelationArea";
     grid-template-columns: 160px 1fr 312px;
   }
 `
